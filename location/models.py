2c9e657a08d52609e6d87e5da11b9cecef38f699
Xavier Gillmann <xgillmann@bluesquarehub.com>
2019-08-11 21:01:32 +0200

    Added link between HealthFacilityGQLType & LocationGQLType + parent Location

--- location/models.py
+++ location/models.py
@@ -1,28 +1,40 @@
 from django.db import models
 from core import fields
 
+
 class Location(models.Model):
     id = models.AutoField(db_column='LocationId', primary_key=True)
-    legacy_id = models.IntegerField(db_column='LegacyId', blank=True, null=True)
-    code = models.CharField(db_column='LocationCode', max_length=8, blank=True, null=True)
-    name = models.CharField(db_column='LocationName', max_length=50, blank=True, null=True)
-    parent = models.IntegerField(db_column='ParentLocationId', blank=True, null=True)
+    legacy_id = models.IntegerField(
+        db_column='LegacyId', blank=True, null=True)
+    code = models.CharField(db_column='LocationCode',
+                            max_length=8, blank=True, null=True)
+    name = models.CharField(db_column='LocationName',
+                            max_length=50, blank=True, null=True)
+    parent = models.ForeignKey('Location', models.DO_NOTHING,
+                               db_column='ParentLocationId',
+                               blank=True, null=True)
     type = models.CharField(db_column='LocationType', max_length=1)
-    validity_from = fields.DateTimeField(db_column='ValidityFrom', blank=True, null=True)
-    validity_to = fields.DateTimeField(db_column='ValidityTo', blank=True, null=True)
+    validity_from = fields.DateTimeField(
+        db_column='ValidityFrom', blank=True, null=True)
+    validity_to = fields.DateTimeField(
+        db_column='ValidityTo', blank=True, null=True)
 
-
-    male_population = models.IntegerField(db_column='MalePopulation', blank=True, null=True)
-    female_population = models.IntegerField(db_column='FemalePopulation', blank=True, null=True)
-    other_population = models.IntegerField(db_column='OtherPopulation', blank=True, null=True)
+    male_population = models.IntegerField(
+        db_column='MalePopulation', blank=True, null=True)
+    female_population = models.IntegerField(
+        db_column='FemalePopulation', blank=True, null=True)
+    other_population = models.IntegerField(
+        db_column='OtherPopulation', blank=True, null=True)
     families = models.IntegerField(db_column='Families', blank=True, null=True)
 
     # rowid = models.TextField(db_column='RowId')
-    audit_user_id = models.IntegerField(db_column='AuditUserId', blank=True, null=True)
+    audit_user_id = models.IntegerField(
+        db_column='AuditUserId', blank=True, null=True)
 
     class Meta:
         managed = False
         db_table = 'tblLocations'
+
 
 class HealthFacility(models.Model):
     id = models.AutoField(db_column='HfID', primary_key=True)
@@ -30,13 +42,14 @@ class HealthFacility(models.Model):
         db_column='LegacyID', blank=True, null=True)
 
     code = models.CharField(db_column='HFCode', max_length=8)
+    name = models.CharField(db_column='HFName', max_length=100)
     acc_code = models.CharField(
         db_column='AccCode', max_length=25, blank=True, null=True)
-    name = models.CharField(db_column='HFName', max_length=100)
     # legalform = models.ForeignKey('Tbllegalforms', models.DO_NOTHING, db_column='LegalForm')
     level = models.CharField(db_column='HFLevel', max_length=1)
     # sublevel = models.ForeignKey('Tblhfsublevel', models.DO_NOTHING, db_column='HFSublevel', blank=True, null=True)
-    # locationid = models.ForeignKey('Tbllocations', models.DO_NOTHING, db_column='LocationId')
+    location = models.ForeignKey(
+        Location, models.DO_NOTHING, db_column='LocationId')
     address = models.CharField(
         db_column='HFAddress', max_length=100, blank=True, null=True)
     phone = models.CharField(
--- location/schema.py
+++ location/schema.py
@@ -1,17 +1,36 @@
+from django.db.models import Q
 import graphene
 from graphene_django import DjangoObjectType
+from graphene_django.filter import DjangoFilterConnectionField
 from .models import HealthFacility, Location
 from .services import HealthFacilityFullPathRequest, HealthFacilityFullPathService
-
-
-class HealthFacilityGQLType(DjangoObjectType):
-    class Meta:
-        model = HealthFacility
+from core import prefix_filterset, filter_validity
 
 
 class LocationGQLType(DjangoObjectType):
     class Meta:
         model = Location
+        interfaces = (graphene.relay.Node,)
+        filter_fields = {
+            "id": ["exact"],
+            "code": ["exact", "istartswith", "icontains", "iexact"],
+            "name": ["exact", "istartswith", "icontains", "iexact"],
+            "type": ["exact"],
+            "parent__id": ["exact"],  # can't import itself!
+        }
+
+
+class HealthFacilityGQLType(DjangoObjectType):
+    class Meta:
+        model = HealthFacility
+        interfaces = (graphene.relay.Node,)
+        filter_fields = {
+            "id": ["exact"],
+            "code": ["exact", "istartswith", "icontains", "iexact"],
+            "name": ["exact", "istartswith", "icontains", "iexact"],
+            "level": ["exact"],
+            **prefix_filterset("location__", LocationGQLType._meta.filter_fields)
+        }
 
 
 class HealthFacilityFullPathGQLType(graphene.ObjectType):
@@ -31,6 +50,17 @@ class Query(graphene.ObjectType):
     health_facility_full_path = graphene.Field(
         HealthFacilityFullPathGQLType,
         hfId=graphene.Int(required=True)
+    )
+
+    health_facilities_str = DjangoFilterConnectionField(
+        HealthFacilityGQLType,
+        str=graphene.String()
+    )
+
+    locations_str = DjangoFilterConnectionField(
+        LocationGQLType,
+        tpe=graphene.String(),
+        str=graphene.String(),
     )
 
     def resolve_health_facility_full_path(self, info, **kwargs):
@@ -53,3 +83,20 @@ class Query(graphene.ObjectType):
             region_code=resp.region_code,
             region_name=resp.region_name
         )
+
+    def resolve_health_facilities_str(self, info, **kwargs):
+        filters = [*filter_validity(**kwargs)]
+        str = kwargs.get('str')
+        if str is not None:
+            filters.extend([Q(code__icontains=str) | Q(name__icontains=str)])
+        return HealthFacility.objects.filter(*filters)
+
+    def resolve_locations_str(self, info, **kwargs):
+        filters = [*filter_validity(**kwargs)]
+        str = kwargs.get('str')
+        if str is not None:
+            filters.extend([Q(code__icontains=str) | Q(name__icontains=str)])
+        tpe = kwargs.get('tpe')
+        if tpe is not None:
+            filters.extend([Q(type__exact=tpe)])
+        return Location.objects.filter(*filters)
