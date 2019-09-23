8e7ab00b86db919f1a987a9cf89538bb3b16c2bf
Eric Darchis <darchis@pobox.com>
2019-09-23 11:14:45 +0200

    Formatting for readability

--- location/models.py
+++ location/models.py
@@ -1,5 +1,5 @@
-from django.db import models
 from core import fields
+from django.db import models
 
 
 class Location(models.Model):
@@ -38,8 +38,7 @@ class Location(models.Model):
 
 class HealthFacility(models.Model):
     id = models.AutoField(db_column='HfID', primary_key=True)
-    legacy_id = models.IntegerField(
-        db_column='LegacyID', blank=True, null=True)
+    legacy_id = models.IntegerField(db_column='LegacyID', blank=True, null=True)
 
     code = models.CharField(db_column='HFCode', max_length=8)
     name = models.CharField(db_column='HFName', max_length=100)
@@ -48,22 +47,16 @@ class HealthFacility(models.Model):
     # legalform = models.ForeignKey('Tbllegalforms', models.DO_NOTHING, db_column='LegalForm')
     level = models.CharField(db_column='HFLevel', max_length=1)
     # sublevel = models.ForeignKey('Tblhfsublevel', models.DO_NOTHING, db_column='HFSublevel', blank=True, null=True)
-    location = models.ForeignKey(
-        Location, models.DO_NOTHING, db_column='LocationId')
-    address = models.CharField(
-        db_column='HFAddress', max_length=100, blank=True, null=True)
-    phone = models.CharField(
-        db_column='Phone', max_length=50, blank=True, null=True)
-    fax = models.CharField(
-        db_column='Fax', max_length=50, blank=True, null=True)
-    email = models.CharField(
-        db_column='eMail', max_length=50, blank=True, null=True)
+    location = models.ForeignKey(Location, models.DO_NOTHING, db_column='LocationId')
+    address = models.CharField(db_column='HFAddress', max_length=100, blank=True, null=True)
+    phone = models.CharField(db_column='Phone', max_length=50, blank=True, null=True)
+    fax = models.CharField(db_column='Fax', max_length=50, blank=True, null=True)
+    email = models.CharField(db_column='eMail', max_length=50, blank=True, null=True)
 
     care_type = models.CharField(db_column='HFCareType', max_length=1)
 
     validity_from = fields.DateTimeField(db_column='ValidityFrom')
-    validity_to = fields.DateTimeField(
-        db_column='ValidityTo', blank=True, null=True)
+    validity_to = fields.DateTimeField(db_column='ValidityTo', blank=True, null=True)
 
     service_pricelist = models.ForeignKey('medical_pricelist.ServicePricelist', models.DO_NOTHING,
                                           db_column='PLServiceID', blank=True, null=True, related_name="health_facilities")
