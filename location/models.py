f8fbd84ba475a5511dc266ad7dbe76d4ca1e6350
Xavier Gillmann <xgillmann@bluesquarehub.com>
2019-09-26 11:30:37 +0200

    added UserDistrict

--- location/models.py
+++ location/models.py
@@ -1,5 +1,6 @@
 from core import fields
 from django.db import models
+from core import models as core_models
 
 
 class Location(models.Model):
@@ -38,7 +39,8 @@ class Location(models.Model):
 
 class HealthFacility(models.Model):
     id = models.AutoField(db_column='HfID', primary_key=True)
-    legacy_id = models.IntegerField(db_column='LegacyID', blank=True, null=True)
+    legacy_id = models.IntegerField(
+        db_column='LegacyID', blank=True, null=True)
 
     code = models.CharField(db_column='HFCode', max_length=8)
     name = models.CharField(db_column='HFName', max_length=100)
@@ -47,16 +49,22 @@ class HealthFacility(models.Model):
     # legalform = models.ForeignKey('Tbllegalforms', models.DO_NOTHING, db_column='LegalForm')
     level = models.CharField(db_column='HFLevel', max_length=1)
     # sublevel = models.ForeignKey('Tblhfsublevel', models.DO_NOTHING, db_column='HFSublevel', blank=True, null=True)
-    location = models.ForeignKey(Location, models.DO_NOTHING, db_column='LocationId')
-    address = models.CharField(db_column='HFAddress', max_length=100, blank=True, null=True)
-    phone = models.CharField(db_column='Phone', max_length=50, blank=True, null=True)
-    fax = models.CharField(db_column='Fax', max_length=50, blank=True, null=True)
-    email = models.CharField(db_column='eMail', max_length=50, blank=True, null=True)
+    location = models.ForeignKey(
+        Location, models.DO_NOTHING, db_column='LocationId')
+    address = models.CharField(
+        db_column='HFAddress', max_length=100, blank=True, null=True)
+    phone = models.CharField(
+        db_column='Phone', max_length=50, blank=True, null=True)
+    fax = models.CharField(
+        db_column='Fax', max_length=50, blank=True, null=True)
+    email = models.CharField(
+        db_column='eMail', max_length=50, blank=True, null=True)
 
     care_type = models.CharField(db_column='HFCareType', max_length=1)
 
     validity_from = fields.DateTimeField(db_column='ValidityFrom')
-    validity_to = fields.DateTimeField(db_column='ValidityTo', blank=True, null=True)
+    validity_to = fields.DateTimeField(
+        db_column='ValidityTo', blank=True, null=True)
 
     service_pricelist = models.ForeignKey('medical_pricelist.ServicePricelist', models.DO_NOTHING,
                                           db_column='PLServiceID', blank=True, null=True, related_name="health_facilities")
@@ -69,3 +77,21 @@ class HealthFacility(models.Model):
     class Meta:
         managed = False
         db_table = 'tblHF'
+
+
+class UserDistrict(models.Model):
+    id = models.AutoField(db_column='UserDistrictID', primary_key=True)
+    legacy_id = models.IntegerField(
+        db_column='LegacyID', blank=True, null=True)
+    user = models.ForeignKey(
+        core_models.InteractiveUser, models.DO_NOTHING, db_column='UserID')
+    location = models.ForeignKey(
+        Location, models.DO_NOTHING, db_column='LocationId')
+    validity_from = fields.DateTimeField(db_column='ValidityFrom')
+    validity_to = fields.DateTimeField(
+        db_column='ValidityTo', blank=True, null=True)
+    audit_user_id = models.IntegerField(db_column='AuditUserID')
+
+    class Meta:
+        managed = False
+        db_table = 'tblUsersDistricts'
--- location/schema.py
+++ location/schema.py
@@ -2,9 +2,10 @@ from django.db.models import Q
 import graphene
 from graphene_django import DjangoObjectType
 from graphene_django.filter import DjangoFilterConnectionField
-from .models import HealthFacility, Location
+from .models import HealthFacility, Location, UserDistrict
 from .services import HealthFacilityFullPathRequest, HealthFacilityFullPathService
 from core import prefix_filterset, filter_validity
+from core import models as core_models
 
 
 class LocationGQLType(DjangoObjectType):
@@ -33,6 +34,23 @@ class HealthFacilityGQLType(DjangoObjectType):
         }
 
 
+class UserDistrictGQLType(graphene.ObjectType):
+    id = graphene.Int()
+    code = graphene.String()
+    name = graphene.String()
+    region_id = graphene.Int()
+    region_code = graphene.String()
+    region_name = graphene.String()
+
+    def __init__(self, district):
+        self.id = district.location.id
+        self.code = district.location.code
+        self.name = district.location.name
+        self.region_id = district.location.parent.id
+        self.region_code = district.location.parent.code
+        self.region_name = district.location.parent.name
+
+
 class HealthFacilityFullPathGQLType(graphene.ObjectType):
     hf_id = graphene.Int()
     hf_code = graphene.String()
@@ -46,22 +64,27 @@ class HealthFacilityFullPathGQLType(graphene.ObjectType):
     region_name = graphene.String()
 
 
+def userDistricts(user):
+    return UserDistrict.objects                 \
+        .select_related('location')             \
+        .select_related('location__parent')     \
+        .filter(user=user)
+
+
 class Query(graphene.ObjectType):
     health_facilities = DjangoFilterConnectionField(HealthFacilityGQLType)
     health_facility_full_path = graphene.Field(
         HealthFacilityFullPathGQLType,
         hfId=graphene.Int(required=True)
     )
-
+    user_districts = graphene.List(
+        UserDistrictGQLType
+    )
     health_facilities_str = DjangoFilterConnectionField(
         HealthFacilityGQLType,
-        str=graphene.String()
-    )
-
-    locations_str = DjangoFilterConnectionField(
-        LocationGQLType,
-        tpe=graphene.String(),
         str=graphene.String(),
+        region_id=graphene.Int(),
+        district_id=graphene.Int(),
     )
 
     def resolve_health_facility_full_path(self, info, **kwargs):
@@ -85,19 +108,26 @@ class Query(graphene.ObjectType):
             region_name=resp.region_name
         )
 
+    def resolve_user_districts(self, info, **kwargs):
+        if info.context.user.is_anonymous:
+            raise NotImplementedError(
+                'Anonymous Users are not registered for districts')
+        if not isinstance(info.context.user._u, core_models.InteractiveUser):
+            raise NotImplementedError(
+                'Only Interactive Users are registered for districts')
+        return [UserDistrictGQLType(d) for d in userDistricts(info.context.user._u)]
+
     def resolve_health_facilities_str(self, info, **kwargs):
         filters = [*filter_validity(**kwargs)]
         str = kwargs.get('str')
         if str is not None:
-            filters.extend([Q(code__icontains=str) | Q(name__icontains=str)])
+            filters += [Q(code__icontains=str) | Q(name__icontains=str)]
+        district_id = kwargs.get('district_id')
+        if district_id is not None:
+            filters += [Q(location__id=district_id)]
+        region_id = kwargs.get('region_id')
+        if region_id is not None:
+            filters += [Q(location__parent__id=region_id)]
+        dist = userDistricts(info.context.user._u)
+        filters += [Q(location__id__in=[l.location.id for l in dist])]
         return HealthFacility.objects.filter(*filters)
-
-    def resolve_locations_str(self, info, **kwargs):
-        filters = [*filter_validity(**kwargs)]
-        str = kwargs.get('str')
-        if str is not None:
-            filters.extend([Q(code__icontains=str) | Q(name__icontains=str)])
-        tpe = kwargs.get('tpe')
-        if tpe is not None:
-            filters.extend([Q(type__exact=tpe)])
-        return Location.objects.filter(*filters)
