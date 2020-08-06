fd01d6c2fed2534d4286f34275a3ff5558e51f47
Xavier Gillmann <xgillmann@bluesquarehub.com>
2020-08-06 17:55:30 +0200

    lock row on mutation

--- location/gql_queries.py
+++ location/gql_queries.py
@@ -7,6 +7,8 @@ from location.models import HealthFacilityLegalForm, Location, HealthFacilitySub
 
 
 class LocationGQLType(DjangoObjectType):
+    client_mutation_id = graphene.String()
+
     class Meta:
         model = Location
         interfaces = (graphene.relay.Node,)
@@ -19,6 +21,11 @@ class LocationGQLType(DjangoObjectType):
             "parent__uuid": ["exact"],  # can't import itself!
             "parent__id": ["exact"],  # can't import itself!
         }
+
+    def resolve_client_mutation_id(self, info):
+        location_mutation = self.mutations.select_related(
+            'mutation').filter(mutation__status=0).first()
+        return location_mutation.mutation.client_mutation_id if location_mutation else None
 
     @classmethod
     def get_queryset(cls, queryset, info):
@@ -41,6 +48,8 @@ class HealthFacilityCatchmentGQLType(DjangoObjectType):
 
 
 class HealthFacilityGQLType(DjangoObjectType):
+    client_mutation_id = graphene.String()
+
     class Meta:
         model = HealthFacility
         interfaces = (graphene.relay.Node,)
@@ -61,6 +70,11 @@ class HealthFacilityGQLType(DjangoObjectType):
 
     def resolve_catchments(self, info):
         return self.catchments.filter(validity_to__isnull=True)
+
+    def resolve_client_mutation_id(self, info):
+        health_facility_mutation = self.mutations.select_related(
+            'mutation').filter(mutation__status=0).first()
+        return health_facility_mutation.mutation.client_mutation_id if health_facility_mutation else None
 
 
 class UserRegionGQLType(graphene.ObjectType):
