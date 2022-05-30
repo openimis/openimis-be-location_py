db9657da4e20942bc521a3cdbb2ebfd5e7622942
sniedzielski <sniedzielski@soldevelo.com>
2022-05-30 12:56:18 +0200

    OTC-607: added 'create health facility'  helpers for batch-run tests

--- location/test_helpers.py
+++ location/test_helpers.py
@@ -1,4 +1,9 @@
-from location.models import Location, UserDistrict
+from location.models import (
+    Location,
+    UserDistrict,
+    HealthFacility,
+    HealthFacilityLegalForm
+)
 
 
 def assign_user_districts(user, district_codes):
@@ -27,4 +32,19 @@ def create_test_location(hf_type, valid=True, custom_props={}):
     )
 
 
-
+def create_test_health_facility(code, location_id, valid=True, custom_props={}):
+    return HealthFacility.objects.create(
+        **{
+            "code": "TST-" + code,
+            "level": "H",
+            "legal_form": HealthFacilityLegalForm.objects.filter(code='C').first(),
+            "location_id": location_id,
+            "name": "Test location " + code,
+            "care_type": 'B',
+            "validity_from": "2019-01-01",
+            "validity_to": None if valid else "2019-06-01",
+            "audit_user_id": -1,
+            "offline": False,
+            **custom_props
+        }
+    )
