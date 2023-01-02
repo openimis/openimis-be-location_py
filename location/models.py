4c66257e9dafffb1939e5e3599d2cfabadc0c96a
Hiren Soni <hirensoni913@gmail.com>
2023-01-02 20:14:59 +0300

    National level entities are added in user location specific filter

--- location/models.py
+++ location/models.py
@@ -130,7 +130,9 @@ class Location(core_models.VersionedModel):
     def build_user_location_filter_query(user: core_models.InteractiveUser) -> Q:
         user_districts = UserDistrict.get_user_districts(user)
         return Q(location__in=Location.objects.filter(uuid__in=user_districts.values_list('location__uuid', flat=True))) | Q(
-            location__in=Location.objects.filter(uuid__in=user_districts.values_list('location__parent__uuid', flat=True)))
+            location__in=Location.objects.filter(uuid__in=user_districts.values_list('location__parent__uuid', flat=True))) | Q(
+            location__isnull=True
+        )
 
     class Meta:
         managed = False
