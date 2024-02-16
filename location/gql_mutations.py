b8affcaa8a251dc7ef605fcf5c5c43bcb4eb1318
Meelad0708 <mazizollah@soldevelo.com>
2024-02-16 15:46:26 +0100

    OP-1729: Error translations update.

--- location/gql_mutations.py
+++ location/gql_mutations.py
@@ -78,7 +78,7 @@ class CreateLocationMutation(CreateOrUpdateLocationMutation):
     def async_mutate(cls, user, **data):
 
         if Location.objects.filter(code=data['code'], type=data['type'], validity_to=None).exists():
-            raise ValidationError("Location with this code already exists.")
+            raise ValidationError(_("location.mutation.location_with_code_exists"))
         try:
             return cls.do_mutate(LocationConfig.gql_mutation_create_locations_perms, user, **data)
         except Exception as exc:
