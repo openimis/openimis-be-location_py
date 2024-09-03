edd8333149754c7f2285747f98c1268ef96e8dd3
delcroip <patrick.delcroix@swisstph.ch>
2024-09-03 15:49:35 +0200

    better test helpers

--- location/test_helpers.py
+++ location/test_helpers.py
@@ -19,8 +19,11 @@ def assign_user_districts(user, district_codes):
         )
 
 
-def create_test_location(loc_type, valid=True, custom_props={}):
-    custom_props = {k: v for k, v in custom_props.items() if hasattr(Location, k)}
+def create_test_location(loc_type, valid=True, custom_props=None):
+    if custom_props is None:
+        custom_props = {}
+    else:
+        custom_props = {k: v for k, v in custom_props.items() if hasattr(Location, k)}
     code = "TST-" + loc_type
     if 'code' in custom_props:
         code = custom_props.pop('code')
@@ -40,37 +43,52 @@ def create_test_location(loc_type, valid=True, custom_props={}):
             }
         )
 
-def create_test_village(custom_props={}):
+    
+    
+def create_test_village(custom_props=None):
+    if custom_props is None:
+        custom_props = {}
     custom_props["name"]=custom_props['name'] if "name" in custom_props else "Test Village"
     test_region = create_test_location('R',custom_props={"name": "Region" + custom_props["name"]})
     test_district = create_test_location('D', custom_props={"parent": test_region,"name":"District " + custom_props["name"]})
     test_ward = create_test_location('W', custom_props={"parent": test_district,"name":"Ward" + custom_props["name"]})
     custom_props["parent"]=test_ward
     test_village = create_test_location('V', custom_props=custom_props)
-    
+
     return test_village
 
 
-def create_test_health_facility(code, location_id, valid=True, custom_props={}):
-    custom_props = {k: v for k, v in custom_props.items() if hasattr(HealthFacility, k)}
+def create_test_health_facility(code=None, location_id=None, valid=True, custom_props=None):
+    if custom_props is None:
+        custom_props = {}
+    else:
+        custom_props = {k: v for k, v in custom_props.items() if hasattr(HealthFacility, k)}
 
     if custom_props is not None and 'code' in custom_props:
         code = custom_props.pop('code')
     elif not code:
-        code = 'TST-HF'        
-    hf = HealthFacility.objects.filter(code=code, validity_to__isnull=valid).first()
-    if hf is not None:
+        code = 'TST-HF'
+    if location_id:
+        custom_props['location_id'] = location_id
+    elif (
+        location_id is None and
+        'location' not in custom_props and
+        'location_id' not in custom_props
+    ):
+        location = Location.objects.filter(type='D', validity_to__isnull=True).first()
+        custom_props['location'] = location or create_test_location('D')
+    
+    obj = HealthFacility.objects.filter(code=code, validity_to__isnull=valid).first()
+    if obj is not None:
         if custom_props:
             HealthFacility.objects.filter(id=obj.id).update(**custom_props)
             obj.refresh_from_db()
-        return hf
     else:
-        return HealthFacility.objects.create(
+        obj = HealthFacility.objects.create(
             **{
                 "code": code,
                 "level": "H",
                 "legal_form": HealthFacilityLegalForm.objects.filter(code='C').first(),
-                "location_id": location_id,
                 "name": "Test location " + code,
                 "care_type": 'B',
                 "validity_from": "2019-01-01",
@@ -80,10 +98,16 @@ def create_test_health_facility(code, location_id, valid=True, custom_props={}):
                 **custom_props
             }
         )
+    # reseting custom props to avoid having it in next calls
+    return obj
 
-def create_test_health_catchment(hf, location, custom_props={}):
-    custom_props = {k: v for k, v in custom_props.items() if hasattr(HealthFacilityCatchment, k)}
-    return HealthFacilityCatchment.objects.create(
+
+def create_test_health_catchment(hf, location, custom_props=None):
+    if custom_props is None:
+        custom_props = {}
+    else:
+        custom_props = {k: v for k, v in custom_props.items() if hasattr(HealthFacilityCatchment, k)}
+    obj = HealthFacilityCatchment.objects.create(
         **{
             "location": location,
             "health_facility": hf,
@@ -94,3 +118,5 @@ def create_test_health_catchment(hf, location, custom_props={}):
             **custom_props
         }
     )
+    
+    return obj
